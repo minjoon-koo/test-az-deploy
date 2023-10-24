7f1460e02970211204c7dbcba93e66fb0943ad21
minjoon-koo <115975651+minjoon-koo@users.noreply.github.com>
2023-10-24 15:16:23 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 echo "Hello World!";
-$testValue = getenv("TEST");
-echo "TEST Variable Value: " . $testValue;
+$aValue = getenv("a");
+echo "a Variable Value: " . $aValue;
+$bValue = getenv("b");
+echo "b Variable Value: " . $bValue;
+$cValue = getenv("c");
+echo "c Variable Value: " . $cValue;
+$dValue = getenv("d");
+echo "d Variable Value: " . $dValue;
+$eValue = getenv("e");
+echo "e Variable Value: " . $eValue;
+$fValue = getenv("f");
+echo "f Variable Value: " . $fValue;
+$gValue = getenv("g");
+echo "g Variable Value: " . $gValue;
+$hValue = getenv("h");
+echo "h Variable Value: " . $hValue;
+$iValue = getenv("i");
+echo "i Variable Value: " . $iValue;
+
 ?>
