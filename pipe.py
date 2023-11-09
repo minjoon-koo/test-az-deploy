a8e10fecfdc980724f279b98c7b01b6f1f632873
minjoon-koo <115975651+minjoon-koo@users.noreply.github.com>
2023-11-09 13:53:46 +0900

    Create pipe.py

---
+++ pipe.py
@@ -0,0 +1,6 @@
+with open('vault_secret_name.txt', 'r') as file:
+    for line in file:
+        text = f"echo $({line.strip()}) >> .env"
+        print(text)
+        with open('create_env.sh', 'a') as file:
+            file.write(f"\n{text}")
