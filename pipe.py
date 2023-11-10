2089e3d85de88afd328dca38db97d0ca331e0de4
minjoon-koo <115975651+minjoon-koo@users.noreply.github.com>
2023-11-10 15:09:10 +0900

    Update pipe.py

--- pipe.py
+++ pipe.py
@@ -1,8 +1,8 @@
 import os
 with open('vault_secret_name.txt', 'r') as file:
     for line in file:
-        text = f"echo $({line.strip()}) >> .env"
-        os.system(f"echo \$$({line.strip()})")
+        text = f"echo $({line.strip()})"
+        #os.system(f"echo \$$({line.strip()})")
         
         with open('create_env.sh', 'a') as file:
             file.write(f"\n{text}")
